a3d6e8ffcf7a777037ef7441a25baac7a8d8b7ad
Jasper den Ouden <o.jasper@gmail.com>
2015-08-16 03:52:25 +0200

    update

--- ToxChannel.lua
+++ ToxChannel.lua
@@ -1,4 +1,4 @@
---  Copyright (C) 09-08-2015 Jasper den Ouden.
+--  Copyright (C) 14-08-2015 Jasper den Ouden.
 --
 --  This is free software: you can redistribute it and/or modify
 --  it under the terms of the GNU General Public License as published
@@ -15,31 +15,49 @@ for k,v in pairs(ToxFriend) do ToxChannel[k] = ToxFriend[k] end
 ToxChannel.__name = "ToxChannel"
 ToxChannel.__index = ToxChannel
 
--- Note ToxChannel.new(a_ToxFriend) should upgrade the friend to a channel
 function ToxChannel.new(self)
    self.call_nr = 0
    self.cdata = self.tox.cdata
    self.current = {}
    self.return_cb = {}
+   self.funs = {}
    return setmetatable(self, ToxChannel)
 end
 
--- Just in case one might want to change them,
+-- Upgrades a ToxFriend to a channel.
+function ToxChannel.from_Friend(self)
+   if getmetatable(self) == ToxChannel then
+      return self
+   else
+      -- Add all the info needed.
+      self.call_nr = 0
+      self.cdata = self.tox.cdata
+      self.current = {}
+      self.return_cb = {}
+      self.funs = {}
+      return ToxChannel.new(self)
+   end
+end
+
+-- Just in case one might want to change them.
 local json = require "json"
 ToxChannel.encode = json.encode
 ToxChannel.decode = json.decode
 
 ToxChannel.call_file_kind = 1023
 
-function ToxChannel:call(name, data, return_callback)
+function ToxChannel:call(name, return_callback)
    self.call_nr = self.call_nr + 1
    -- Register the function that has to respond on-return.
    self.return_cb[self.call_nr] = return_callback
 
-   local json = self.encode(data)
-   local nr = self:file_send(self.call_file_kind, #json,
-                             string.format("fm:%s:%x", name, self.call_nr))
-   self:file_whole_data(nr, json)
+   return function(...)
+      local json = self.encode{...}
+      assert(type(self.call_file_kind) == "number")
+      local nr = self:file_send(self.call_file_kind, #json,
+                                string.format("fn:%s:%x", name, self.call_nr))
+      self:file_whole_data(nr, json)
+   end
 end
 
 -- Send return values.
@@ -51,8 +69,8 @@ function ToxChannel:ret(name, data, call_nr)
 end
 
 local function name_and_number(filename)
-   local _, i1   = string.find(filename, ":", 1, true)
-   local _, i2   = string.find(filename, ":", i1 + 1, true)
+   local _, i1 = string.find(filename, ":", 1, true)
+   local _, i2 = string.find(filename, ":", i1 + 1, true)
    return string.sub(filename, i1 + 1, i2 - 1), tonumber(string.sub(filename, i2 + 1), 16)
 end
 
@@ -74,12 +92,12 @@ function ToxChannel:cb_file_recv_chunk(file_nr, pos, data, len)
          local ret_cb = self.return_cb[cur.call_nr]
          if ret_cb then  -- It is a return value.
             assert(cur.is_ret)
-            ret_cb(unpack(self.decode(cur.json)))
+            ret_cb(self, unpack(self.decode(cur.json)))
             return
          end
-         local fun = if self.funs[cur.name]  -- If there is a function for this, use it.
+         local fun = self.funs[cur.name]  -- If there is a function for this, use it.
          if fun then
-            local result = fun(unpack(self.decode(cur.json)))
+            local result = fun(self, unpack(self.decode(cur.json)))
             if self.has_return[cur.name] then  -- If return result for this.
                self:ret(cur.name, result)
             end
@@ -97,3 +115,4 @@ end
 --typedef void tox_file_recv_chunk_cb(Tox *tox, uint32_t friend_number, uint32_t file_number, uint64_t position, const uint8_t *data, size_t length, void *user_data)
 --typedef void tox_file_recv_control_cb(Tox *tox, uint32_t friend_number, uint32_t file_number, TOX_FILE_CONTROL control, void *user_data)
 
+return ToxChannel
