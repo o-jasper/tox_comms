70f1f1424620606100043c15c90211c1754507ea
Jasper den Ouden <o.jasper@gmail.com>
2015-08-18 18:08:13 +0200

    split off `cb_chunk`, renamed `kind` to `name` for channelling data, added the name to the multi-chunk approach

--- ToxChannelMsg.lua
+++ ToxChannelMsg.lua
@@ -43,11 +43,12 @@ local json = require "json"
 ToxChannelMsg.chunk_max_len = 1372
 ToxChannelMsg.send_chunk = ToxChannelMsg.send_message
 
-function ToxChannelMsg:channel_data(data, way, cb_data)
+function ToxChannelMsg:channel_data(name, data)
    local nr = self.data_nr + 1
    self.data_nr = nr
-   local header = string.format("~~%x:%s:", nr, way)
+   local header = string.format("~~%x:%s:", nr, name)
    if #header + #data > self.chunk_max_len then  -- Cant send at once.
+      data = name .. ":" .. data  -- Still prepend name
       local n, j = 0, 0
       while j < #data do
          local chunk = string.format("~~%x:%x:", nr,n)
@@ -56,59 +57,64 @@ function ToxChannelMsg:channel_data(data, way, cb_data)
          j = j + sendlen
          n = n + 1
       end
-      self:send_chunk(string.format("~~%x:N=%x", nr, n))
-      -- TODO send the number of chunks in the entire thing.
-      --return false  -- TODO not yet implemented
+      -- The number, so we can get them all.
+      self:send_chunk(string.format("~~%x:N=%x:%s", nr, n, name))
    else  -- Just send it.
       self:send_chunk(header .. data)
    end
    return nr
 end
 
-function ToxChannelMsg:cb_message(tp, msg)
-   local msg = ffi.string(msg)
-   if tp == 0 then
-      if string.find(msg, "^~~[%x]+:N=[%x]+$") then  -- Length indicator
-         local j1 = string.find(msg, ":", 1, true)
-         local _, j2 = string.find(msg, ":N=", j1, true)
+-- TODO annoying they're mostly getting at the same data..
+function ToxChannelMsg:cb_chunk(data)
+   if string.find(data, "^~~[%x]+:N=[%x]+$") then  -- Length indicator
+      local j1 = string.find(data, ":", 1, true)
+      local _, j2 = string.find(data, ":N=", j1+1, true)
 
-         local nr = tonumber(string.sub(msg, 2, j1-1), 16)
-         local chunks = self.chunks[nr] or {}
-         self.chunks[nr] = chunks
-         
-         self.chunks.n = tonumber(string.sub(msg, j2+1), 16)
-      elseif string.find(msg, "^~~[%x]+:[%x]+:") then
-         local j1 = string.find(msg, ":", 1, true)
-         local j2 = string.find(msg, ":", j1 + 1, true)
-               
-         local nr = tonumber(string.sub(msg, 2, j1-1), 16)  -- Index of the message.
-         local n  = tonumber(string.sub(msg, j1+1, j2-1), 16) -- Index of the chunk.
-               
-         local chunks = self.chunks[nr] or {}
-         self.chunks[nr] = chunks
-         chunks[n] = string.sub(msg, j2+1)
-         if chunks.n then
-            local i = 0
-            while i < chunks.n do
-               if not chunks[i] then
-                  return  -- Don't have them all.
-               end
-               i = i + 1
-            end
-            -- Have it all, put it together, can call it.
-            fun(self, nr, table.concat(chunks))
+      local nr = tonumber(string.sub(data, 2, j1-1), 16)
+      local chunks = self.transfers[nr] or {}
+      self.transfers[nr] = chunks
+
+      chunks.n = tonumber(string.sub(data, j2+1), 16)
+   elseif string.find(data, "^~~[%x]+:[%x]+:") then
+      local j1 = string.find(data, ":", 1, true)
+      local j2 = string.find(data, ":", j1 + 1, true)
+
+      local nr = tonumber(string.sub(data, 2, j1-1), 16)  -- Index of the message.
+      local n  = tonumber(string.sub(data, j1+1, j2-1), 16) -- Index of the chunk.
+
+      local chunks = self.transfers[nr] or {}
+      self.transfers[nr] = chunks
+      chunks.got_cnt = (chunks.got_cnt or 0) + 1
+      chunks[n] = string.sub(data, j2+1)
+      if chunks.n and chunks.n == chunks.got_cnt then
+         -- Have it all, put it together, can call it.
+         local data = table.concat(chunks)
+         local j = string.find(data, ":", 1, true)
+         local name = string.sub(data, 0,j-1)
+         local fun = self.data_cb[name]
+         if fun then  -- If has function, get it.
+            fun(self, nr, string.sub(data, j+1))
          end
-      else
-         local _, j1 = string.find(msg, "^~~[%x]+:")
-         local j2, j3 = string.find(msg, ":", j1+1, true)
-         
-         local kind = string.sub(msg, j1+1,j2-1)  -- Kind of message.
-         local fun = self.data_cb[kind]
-         if fun then
-            local nr = tonumber(string.sub(msg, 3, j1-1), 16)  -- Index of the message.
-            fun(self, nr, string.sub(msg, j3+1))
-         end
+         -- clean up.
+         self.transfers[nr] = nil
       end
+   else
+      local _, j1 = string.find(data, "^~~[%x]+:")
+      local j2, j3 = string.find(data, ":", j1+1, true)
+
+      local name = string.sub(data, j1+1,j2-1)  -- Kind of message.
+      local fun = self.data_cb[name]
+      if fun then
+         local nr = tonumber(string.sub(data, 3, j1-1), 16)  -- Index of the message.
+         fun(self, nr, string.sub(data, j3+1))
+      end
+   end
+end
+
+function ToxChannelMsg:cb_message(tp, msg)
+   if tp == 0 then
+      self:cb_chunk(ffi.string(msg))
    end
 end
 
@@ -123,7 +129,7 @@ function data_cb:json_call(nr, data)
    local fun = self.funs[name]
    if fun then  -- Got something for this.
       if self.returning_funs[name] then  -- Return the resulting value.
-         self:channel_data(json.encode(fun(unpack(args))), "json_ret")
+         self:channel_data("json_ret", json.encode(fun(unpack(args))))
       else  -- Just call the function.
          fun(unpack(args))
       end
@@ -142,7 +148,7 @@ end
 
 function ToxChannelMsg:call(name, return_callback)
    return function(...)
-      local nr = self:channel_data(name .. ":" .. json.encode({...}), "json_call")
+      local nr = self:channel_data("json_call", name .. ":" .. json.encode({...}))
       self.return_cb[nr] = return_callback
    end
 end
