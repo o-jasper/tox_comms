408207afed9baa1e0715ef77a721d2c7f495887c
Jasper den Ouden <o.jasper@gmail.com>
2015-09-22 21:29:50 +0200

    fixups to match other changes

--- Bot/main.lua
+++ Bot/main.lua
@@ -1,23 +1,24 @@
 local Bot = require "tox_comms.Bot"
 
 local bot = Bot:new()
-print("Myself:", bot.tox:addr())
+print("Myself:", bot:addr())
 
 local socket = require "socket"
 
 local stop = false
 if arg[1] then
-   bot:add_friend(arg[1], "bot 10k")
+   bot:friend_add(arg[1], "bot 10k")
 end
 
 bot:save()
 print("First iteration")
-bot.tox:iterate()
+bot:iterate()
 
 print("Going into loop")
 local stopcnt = 100
+local last_t = os.time()
 while stopcnt > 0 do
-   socket.sleep(bot.tox:iteration_interval()/1000.0)
+   socket.sleep(bot:iteration_interval()/1000.0)
    bot:iterate()
    if stop then
       if stopcnt == 100 then print("stopping...") end
