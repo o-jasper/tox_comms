ff62453ba2ce229d4d0244592a99d91fe0fb3ce7
Jasper den Ouden <o.jasper@gmail.com>
2015-10-13 12:15:27 +0200

    Javascript makes the table

--- tox_comms/client/page_html/assets/js/aliasses_update.js
+++ tox_comms/client/page_html/assets/js/aliasses_update.js
@@ -2,10 +2,10 @@ function aliasses_update() {
     var addrs = tox_addrs();
     ge("cnt").textContent = addrs.length;
 
-    var html = ""
+    var html = "<table>"
     for(i in addrs) {  // TODO claims of self?
         html = html + '<tr><td><a href="/contacts/' + addrs[i] + '">' + 
             addrs[i] + '</a></td></tr>';
     }
-    ge("list").innerHTML = html;
+    ge("list").innerHTML = html + "</table>";
 }
