17a2db2f93cdfcf88b19cc23a2963aef30a257d7
Jasper den Ouden <o.jasper@gmail.com>
2015-10-18 20:45:56 +0200

    Stuff for optional addition of delay, got rid of the "touched" stuff

--- tox_client/assets/js/chat.js
+++ tox_client/assets/js/chat.js
@@ -1,17 +1,23 @@
-var touched_since = false;
-
-function touch_chat_input() { touched_since = true; }
+function touch_chat_input(event) {
+    if(event && event.keyCode == 13) {
+        send_chat_input();
+    }
+}
 
 function send_chat_input_cb() {
     ge("chat_input_button").textContent = "s";
-    if(!touched_since){ ge("chat_input").value = ""; }
     chat_update(); 
-    touched_since = false;
 }
 
-//var send_delay = 1; // Might want delaying option.
-function send_chat_input() {
-    touched_since = false;
-    ge("chat_input_button").textContent = "U";
-    callback_send_chat([fa, ta, 0, ge("chat_input").value], send_chat_input_cb);
+var send_delay = false; // Might want delaying option.
+function send_chat_input(dont_delay) {
+    if(!send_delay || dont_delay) {
+        ge("chat_input_button").textContent = "U";
+        callback_send_chat([fa, ta, 0, ge("chat_input").value], send_chat_input_cb);
+        ge("chat_input").value = "";
+    } else {
+        // TODO delay only really useful if you can cancel..
+        ge("chat_input_button").textContent = "W " + send_delay/1000 + "s";
+        setTimeout(function(){ send_chat_input(true); }, send_delay);
+    }
 }
